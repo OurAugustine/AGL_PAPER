fd72c99874a426cc51db68c70132b707375e7360
OUR AUGUSTINE <our.wen.augustine@gmail.com>
2024-08-14 15:40:21 -0500

    deleting
    mistake

--- code/test.py
+++
@@ -1,6 +0,0 @@
-# this is a program to test the linear regression model
-import numpy as np
-import matplotlib.pyplot as plt
-
-# load the data
-data = np.loadtxt('data.txt', delimiter=',')
--- labs/lab0/README.md
+++
@@ -1,4 +0,0 @@
-# Lab 0
-This is a lab for practicing Git commands.. 
-Please type your name below..
-Ouru Augustine
